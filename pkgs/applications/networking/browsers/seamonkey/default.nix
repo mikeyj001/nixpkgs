de2ba8e939c4cd6c9df3ff309b6820891dba81cf
mikeyj001 <mikeyj001@hotmail.com>
2023-12-18 17:30:44 +0000

    Updating Seamonkey to 2.53.18

--- pkgs/applications/networking/browsers/seamonkey/default.nix
+++ pkgs/applications/networking/browsers/seamonkey/default.nix
@@ -1,19 +1,22 @@
 with import <nixpkgs>{};
 
-{ stdenv, lib, bzip2, glib, dbus-glib, xorg, gtk3, gcc-unwrapped, fetchurl, autoPatchelfHook }:
+{ stdenv, lib, bzip2, glib, dbus-glib, xorg, gtk3, gcc-unwrapped,libXdamage, fetchurl, autoPatchelfHook }:
 
 stdenv.mkDerivation rec {
-  version = "2.53.17.1";
+  version = "2.53.18";
   name = "seamonkey-${version}";
 
   src = fetchurl {
 	url = "https://archive.mozilla.org/pub/seamonkey/releases/${version}/linux-x86_64/en-GB/seamonkey-${version}.en-GB.linux-x86_64.tar.bz2";
-	# 2.53.17.1
-#	sha256 = "sha256-JGI6objGvH7gwBAqBm1XRtVLfW7MynhpecxKm61yCbQ=";
-#	sha256 = "";
 	#2.53.17
 #	sha256 = "sha256-SZFcMbuWb6O7lggc1ZRPcdbM1Ca2MIE2PbZjtaijvH8=";
-	sha256 = "sha256-JGI6objGvH7gwBAqBm1XRtVLfW7MynhpecxKm61yCbQ=";
+
+	# 2.53.17.1
+#	sha256 = "sha256-JGI6objGvH7gwBAqBm1XRtVLfW7MynhpecxKm61yCbQ=";
+
+	# 2.53.18
+#	sha256 = "";
+	sha256 = "sha256-SqiPnnFuZ7IEXhBwecxus4NUePQNWR/P9z2xka8bPdM=";
   };
 
   unpacked = ''
@@ -23,28 +26,55 @@ stdenv.mkDerivation rec {
 
   dontConfigure = true;
   dontBuild = true;
-  dontStrip = true;
+   
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    gtk3               # libgtk-3.so.0
+    xorg.libxcb        # libX11-xcb.so.1
+    stdenv.cc.cc.lib   # libstdc++.so.6
+    xorg.libXdamage    # libXdamage.so.1
+    dbus-glib          # libdbus-glib-1.so.2
+    xorg.libXt         # libXt.so.6
+    glib               # libgthread-2.0.so.0
+ ];
+
+
+
   installPhase = ''
     mkdir -p $out/bin
-    cp -av seamonkey $out/bin
+#    mkdir -p $out
+#    cp -av seamonkey $out/bin
+    cp -av * $out/bin
   '';
-  preFixup = let
-    # we prepare our library path in the let clause to avoid it become part of the input of mkDerivation
-    libPath = lib.makeLibraryPath [
-      gtk3               # libgtk-3.so.0
-      xorg.libxcb        # libX11-xcb.so.1
-      stdenv.cc.cc.lib   # libstdc++.so.6
-      xorg.libXdamage    # libXdamage.so.1
-      dbus-glib          # libdbus-glib-1.so.2
-      xorg.libXt         # libXt.so.6
-      glib               # libgthread-2.0.so.0
-    ];
-  in ''
-    patchelf \
-      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${libPath}" \
-      $out/bin/seamonkey
-  '';
+#  preFixup = let
+#    # we prepare our library path in the let clause to avoid it become part of the input of mkDerivation
+#    libPath = lib.makeLibraryPath [ 
+#   [   gtk3               # libgtk-3.so.0
+#       xorg.libxcb        # libX11-xcb.so.1
+#       stdenv.cc.cc.lib   # libstdc++.so.6
+#       xorg.libXdamage    # libXdamage.so.1
+#       dbus-glib          # libdbus-glib-1.so.2
+#       xorg.libXt         # libXt.so.6
+#       glib               # libgthread-2.0.so.0
+#    ]
+#    [
+#        "/nix/store" 
+#    ];
+#  ]
+
+#  in ''
+#    patchelf \
+#      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+#      --set-rpath "${libPath}" \
+#      $out/bin/seamonkey
+#  '';
+  
+  autopatchelf = [ "libgtk-3.so.0" ];
+ 
+  dontStrip = true;
 
   meta = with lib; {
     homepage = "https://www.seamonkey-project.org/";
